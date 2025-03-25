e3631e5fc23d2d3d870e6a85694589316f0ddf3d
EslamMohsen110 <Eslam.mohsen99@gmail.com>
2025-03-25 17:35:57 +0200

    create const color and custom text filed

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_text_filed.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(children: [CustomTextFiled(), CustomTextFiled()]),
+    );
   }
 }
