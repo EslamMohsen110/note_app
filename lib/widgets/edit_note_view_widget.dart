77918e2d4ee0e6fe22ce9f70ac7a3ccbf37b37ca
EslamMohsen110 <Eslam.mohsen99@gmail.com>
2025-03-27 17:37:22 +0200

    create view to edite note view and navigator

---
+++ lib/widgets/edit_note_view_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditNoteViewWidget extends StatelessWidget {
+  const EditNoteViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
